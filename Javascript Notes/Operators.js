014c1aa23ef9ca4ead3a20e139410820728ad029
RiyaZahidShaikh <riyazahidshaikh.official@gmail.com>
2024-02-09 16:36:58 +0530

    Operators

---
+++ Javascript Notes/Operators.js
@@ -0,0 +1,29 @@
+//Arithmetic Operators
+let a = 3;
+let ans = (100 + 50) * a;
+console.log("Arithmetic Operators:",ans);
+
+//Assignment Operator 
+let x = 10;
+x += 5;
+console.log("Assignment Operators:",x);
+
+//Comparison operators 
+//Numbers
+let num1 = 20;
+let num2 = 5;
+let result = num1 < num2;
+console.log("Comparison operator using numbers:",result);
+//Strings
+let text1 = "A";
+let text2 = "B";
+let res = text1 < text2;
+console.log("Comparison operator using strings:",res);
+
+//Logical Operators
+if (num1 > num2 && text1 < text2){
+    console.log("Logical Operator '&&' is Successfully used...");
+}
+
+//Bitwise Operators
+console.log("Bitwise Operatoration on 5 & 1 :",5 & 1);
